e6af5c410437c0c55a06d02366551641e918ce62
Daniel Negassi <dyn29@hotmail.com>
2018-07-10 07:24:41 -0500

    Routes created, Data Enhanced

--- Superheros/server.js
+++ Superheros/server.js
@@ -1,6 +1,27 @@
 const express = require('express');
-const app     = express();
-const superheroes = ['batman', 'superman', 'hulk'];
+const app = express();
+const superheroes = [{
+    name: 'Batman',
+    powers: ['flight', 'invulnerability', 'x-ray vision']
+  },
+  {
+    name: "superman",
+    powers: ['flight', 'invulnerability', 'x-ray vision']
+  },
+  {
+    name: 'hulk',
+    powers: ['flight', 'invulnerability', 'x-ray vision']
+  },
+];
+
+app.get('/superheroes', (req, res) => {
+  res.send(superheroes)
+});
+
+app.get('/superheroes/:index', (req, res) => {
+  console.log(req.params, '<-- this is req.params');
+  res.send(superheroes[req.params.index]);
+});
 
 
 
